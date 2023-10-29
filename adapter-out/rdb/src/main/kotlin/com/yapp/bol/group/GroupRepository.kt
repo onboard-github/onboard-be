46c02d62d574bb1d95eedcdf55e6b4c08ab51b4f
seung-00 <seung-00@naver.com>
2023-10-29 11:40:57 +0900

    BOL-397 feat: 그룹 검색시 그룹명이 겹치는 경우를 소속명 겹치는 경우보다 더 우선적으로 나타나게 표시

--- adapter-out/rdb/src/main/kotlin/com/yapp/bol/group/GroupRepository.kt
+++ adapter-out/rdb/src/main/kotlin/com/yapp/bol/group/GroupRepository.kt
@@ -4,10 +4,25 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Slice
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 internal interface GroupRepository : JpaRepository<GroupEntity, Long> {
-
-    fun findByNameLikeOrOrganizationLike(name: String, organization: String, pageable: Pageable): Slice<GroupEntity>
+    @Query("""
+        SELECT g FROM GroupEntity g
+        WHERE (g.name LIKE :name OR g.organization LIKE :organization)
+        ORDER BY 
+            CASE WHEN g.name = :exactName THEN 1 
+                 WHEN g.organization = :exactOrganization THEN 2
+                 ELSE 3 END,
+            g.createdDate DESC
+    """)
+    fun findByNameOrOrganizationWithPriority(
+        @Param("name") name: String,
+        @Param("organization") organization: String,
+        @Param("exactName") exactName: String,
+        @Param("exactOrganization") exactOrganization: String,
+        pageable: Pageable
+    ): Slice<GroupEntity>
 
     @Query("SELECT g FROM MemberEntity m JOIN FETCH GroupEntity g ON m.groupId = g.id WHERE m.userId=:userId")
     fun findByUserId(userId: Long): List<GroupEntity>
