3a689b8e741e0dc2b8d16ee34465bd1da977a91d
HoYeon Lee <comforest@naver.com>
2023-12-13 19:41:36 +0900

    ONB-88 fix: 그룹 생성 안되는 버그 수정

--- adapter-out/rdb/src/main/kotlin/com/yapp/bol/group/GroupCommandRepositoryImpl.kt
+++ adapter-out/rdb/src/main/kotlin/com/yapp/bol/group/GroupCommandRepositoryImpl.kt
@@ -1,16 +1,20 @@
 package com.yapp.bol.group
 
+import com.yapp.bol.NotFoundFileException
+import com.yapp.bol.file.FileRepository
 import com.yapp.bol.group.dto.GroupMemberList
 import com.yapp.bol.group.member.MemberList
 import com.yapp.bol.group.member.MemberRepository
 import com.yapp.bol.group.member.OwnerMember
 import com.yapp.bol.group.member.toDomain
 import com.yapp.bol.group.member.toEntity
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 
 @Repository
 internal class GroupCommandRepositoryImpl(
+    private val fileRepository: FileRepository,
     private val groupRepository: GroupRepository,
     private val memberRepository: MemberRepository,
 ) : GroupCommandRepository {
@@ -25,4 +29,13 @@ internal class GroupCommandRepositoryImpl(
 
         return GroupMemberList(groupEntity.toDomain(), memberList)
     }
+
+    private fun Group.toEntity(): GroupEntity = GroupEntity(
+        id = id,
+        name = name,
+        description = description,
+        organization = organization,
+        profileImage = fileRepository.findByIdOrNull(profileImage.id.value) ?: throw NotFoundFileException,
+        accessCode = accessCode,
+    )
 }
