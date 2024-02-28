ddd9b5349126486eca0ffbf68a9751cbf2fc7c3f
HoYeon Lee <comforest@naver.com>
2024-02-28 18:39:39 +0900

    ONB-225 feat: 어드민 카카오 로그인

--- adapter-in/admin/frontend/src/router/MainRouter.js
+++ adapter-in/admin/frontend/src/router/MainRouter.js
@@ -5,13 +5,19 @@ import App from "../App";
 import GroupList from "../view/group/GroupList";
 import Auth from "../view/auth/Auth";
 import {usePermissionState} from "../view/auth/AuthUtils";
+import {KakaoRedirect} from "../view/auth/KakaoRedirect";
 
 export default function MainRouter() {
 
     const permission = usePermissionState()
 
     if (permission.groupList == null || permission.groupList.length === 0)
-        return <Auth/>
+        return  (
+                <Routes>
+                    <Route path='/auth/kakao' element={<KakaoRedirect/>}/>
+                    <Route path='/*' element={<Auth/>}/>
+                </Routes>
+        );
 
     return (
             <Routes>
--- adapter-in/admin/frontend/src/view/auth/Auth.js
+++ adapter-in/admin/frontend/src/view/auth/Auth.js
@@ -1,12 +1,13 @@
 import GoogleLoginButton from "./GoogleLoginButton";
 import {isLogin, usePermissionState} from "./AuthUtils";
 import RegisterAdmin from "./RegisterAdmin";
+import KakaoLoginButton from "./KakaoLoginButton";
 
 export default function Auth() {
     const permission = usePermissionState()
 
     if (!isLogin())
-        return <GoogleLoginButton/>
+        return <KakaoLoginButton/>
 
     if (permission.groupList.length === 0)
         return <RegisterAdmin/>
